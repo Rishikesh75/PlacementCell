29567fe7135f19d80979d3c9e0f02c93ee41bc66
Rishikesh27 <kotlarishikesh27@gmail.com>
2026-06-28 17:56:52 +0530

    Moving the backend code from aspnet to spring.

---
+++ PlacementicsBackend/src/main/java/com/example/placementicsbackend/PlacementicsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.placementicsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementicsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacementicsBackendApplication.class, args);
+    }
+
+}
---
+++ PlacementicsBackend/src/test/java/com/example/placementicsbackend/PlacementicsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placementicsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementicsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
